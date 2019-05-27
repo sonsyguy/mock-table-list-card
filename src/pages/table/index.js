3c2ac2827f283667b4805b1b016f2141f3d8fea8
whatever <whatever@whateverdeMacBook-Pro.local>
2019-05-27 21:42:43 +0800

    edit-model-table

--- src/pages/table/index.js
+++ src/pages/table/index.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react'
-import {Table,Popover,Icon,} from 'antd'
+import {Table,Popover,Icon,Modal,Form,Input} from 'antd'
 import styles from './index.css';
 
 const data = [];
@@ -13,15 +13,62 @@ for (let i = 0; i < 46; i++) {
   });
 }
 
-const edit = (record,text) => {
-  console.log(record,text);
-}
+
 class TableShow extends Component {
-  constructor (props){
-    super(props)
+  state = {
+    visible:false,
+    editData:{},
+    formLayout: 'horizontal',
   }
+
+  edit = (record,text) => {
+    let {editData} = this.state;
+    editData = record;
+    this.setState({
+      visible: true,
+      editData,
+    },);
+    this.props.form.setFieldsValue({
+      username: editData.name,
+    });
+    this.props.form.setFieldsValue({
+      password: editData.address,
+    });
+    console.log(editData);
+  }
+
+  handleSubmit = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+      editData:{}
+    });
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+      editData:{}
+    });
+  };
   
   render (){
+    const { getFieldDecorator } = this.props.form;
+    const { formLayout } = this.state;
+    const formItemLayout =
+      formLayout === 'horizontal'
+        ? {
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 },
+          }
+        : null;
     const columns = [
       {
         title: 'Name',
@@ -54,7 +101,7 @@ class TableShow extends Component {
       {
         title: 'Action',
         render: (record,text) => {
-          return(<a onClick={() => edit(record,text)}>delete</a>)
+          return(<a onClick={() => this.edit(record,text)}>delete</a>)
         }
       },
     ];
@@ -72,9 +119,43 @@ class TableShow extends Component {
       )
     }
     return(
-      <Table rowKey="name" columns={columns} dataSource={data} expandedRowRender={expandedRowRender} />
+      <div>
+        <Table rowKey="name" columns={columns} dataSource={data} expandedRowRender={expandedRowRender} />
+        <Modal
+            title="Basic Modal"
+            visible={this.state.visible}
+            onOk={this.handleSubmit}
+            onCancel={this.handleCancel}
+          >
+          <p>{this.state.editData.age}</p>
+          <p>{this.state.editData.name}</p>
+          <p>{this.state.editData.address}</p>
+          <Form className="login-form"  layout={formLayout}>
+            <Form.Item label="username" {...formItemLayout}>
+              {getFieldDecorator('username', {
+                rules: [{message: 'Please input your username!' }],
+              })(
+                <Input
+                  placeholder="Username"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item label="password" {...formItemLayout}>
+              {getFieldDecorator('password', {
+                rules: [{message: 'Please input your Password!' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
     )
   }
 }
 
-export default TableShow
+export default Form.create({ name: 'editTable' })(TableShow)
